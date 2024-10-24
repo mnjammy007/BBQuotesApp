957303847fc21e91510faff1da8d8394106adbe0
mnjammy007 <naseem4954r@gmail.com>
2024-10-24 10:43:18 +0530

    Initial Commit

---
+++ BBQuotes18/BBQuotes18App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes18App.swift
+//  BBQuotes18
+//
+//  Created by Apple on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes18Tests/BBQuotes18Tests.swift
@@ -0,0 +1,16 @@
+//
+//  BBQuotes18Tests.swift
+//  BBQuotes18Tests
+//
+//  Created by Apple on 24/10/24.
+//
+
+import Testing
+
+struct BBQuotes18Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
